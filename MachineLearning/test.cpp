feeea226811e3188fb048057442a620bd80c1a14
thomasfouan <thomasfouan@hotmail.fr>
2018-03-05 16:23:54 +0100

    first commit

---
+++ MachineLearning/test.cpp
@@ -0,0 +1,15 @@
+//
+//  test.cpp
+//  MachineLearning
+//
+//  Created by Thomas Fouan on 05/03/2018.
+//  Copyright © 2018 Thomas Fouan. All rights reserved.
+//
+
+#include <stdio.h>
+
+extern "C" {
+    int add_to_42(int nb) {
+        return 42 + nb;
+    }
+}
