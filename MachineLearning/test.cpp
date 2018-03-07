f66a81fa316204f3285ca1e8b8b22fcf43582882
thomasfouan <thomasfouan@hotmail.fr>
2018-03-07 14:04:22 +0100

    made few changes

--- MachineLearning/test.cpp
+++ MachineLearning/test.cpp
@@ -15,7 +15,7 @@ extern "C" {
         return 42 + nb;
     }
     
-    double get_current_weights_value(double* w, double* params, int length) {
+    int get_current_weights_value(double* w, double* params, int length) {
         double res = 0;
         int i = 0;
         
@@ -23,13 +23,10 @@ extern "C" {
             res += w[i] * params[i];
         }
         
-        return res;
+        return res < 0 ? -1 : 1;
     }
     
     double* linear_create(int nbParameters) {
-        //int nbElement = 100;
-        //int length = nbParameters * nbElement;
-        //double* w = (double*) malloc(sizeof(double) * length);
         double* w = (double*) malloc(sizeof(double) * nbParameters);
         int i;
         
@@ -48,10 +45,13 @@ extern "C" {
         return NULL;
     }
     
-    void linear_train_classification(double* w, int wSize, double* parameters, int nbParameters, int parametersLength, double* results, int resultsLength, int alpha) {
+    void linear_train_classification(double* w, int wSize, double* parameters, int nbParameters, int parametersLength, double* results, int resultsLength, double alpha, int maxIter) {
         double** x = (double**) malloc(sizeof(double*) * parametersLength);
         int i, j;
         int stop = 0;
+        //bool oneDifferent = false;
+        
+        srand(time(NULL));
         
         for (i = 0; i < parametersLength; i++) {
             x[i] = (double*) malloc(sizeof(double) * nbParameters + 1);
@@ -64,13 +64,19 @@ extern "C" {
         }
         
         do {
-            for (i = 0; i < parametersLength; i++) {
-                for (j = 0; j < wSize; j++) {
-                    double diff = results[i] - get_current_weights_value(w, x[i], wSize);
-                    w[i] = w[i] + alpha * (diff) * x[i][j];
-                }
+            int index = rand()%parametersLength;
+            for (j = 0; j < wSize; j++) {
+                int currentRes = get_current_weights_value(w, x[index], wSize);
+                double diff = results[index] - currentRes;
+                
+                w[j] = w[j] + alpha * (diff) * x[index][j];
             }
             stop++;
-        } while(stop < 100);
+        } while(stop < maxIter);
+        
+        for (i = 0; i < nbParameters + 1; i++) {
+            free(x[i]);
+        }
+        free(x);
     }
 }
